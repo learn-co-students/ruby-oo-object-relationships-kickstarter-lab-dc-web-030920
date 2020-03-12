cc85efbf920ebcd22663543ffb0cb7714cdbfee1
kaidornel <kai_dornel@hotmail.com>
2020-03-12 15:46:15 -0400

    iinitial commit

---
+++ lib/project_backer.rb
@@ -0,0 +1,14 @@
+class ProjectBacker
+
+    @@all = []
+    attr_reader :project, :backer
+
+    def initialize(project, backer)
+        
+    end
+
+
+    def self.all
+        @@all
+    end
+end
